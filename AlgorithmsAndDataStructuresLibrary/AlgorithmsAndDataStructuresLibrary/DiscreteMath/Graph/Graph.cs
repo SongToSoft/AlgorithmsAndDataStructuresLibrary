ebe10cbc36e3a0f915b9bb9a2037865c3103f635
Bogdanov Dima <quy888@yandex.ru>
2019-07-16 15:20:31 +1000

    Upd project
    Upd project

---
+++ AlgorithmsAndDataStructuresLibrary/AlgorithmsAndDataStructuresLibrary/DiscreteMath/Graph/Graph.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsAndDataStructuresLibrary.DiscreteMath.Graph
+{
+    namespace DiscreteMath
+    {
+        public enum EConnected
+        {
+            DOUBLY_CONNECTED,
+            SINGLE_CONNECTED
+        }
+
+        public class Graph
+        {
+            private Vertex[] m_Vertexes;
+            private List<int> m_Way;
+            private Queue<int> m_Queue;
+            private EConnected m_Connected;
+            private float[] m_Distance;
+
+            public Graph(int numberVertex, EConnected connected = EConnected.DOUBLY_CONNECTED)
+            {
+                m_Connected = connected;
+                m_Vertexes = new Vertex[numberVertex];
+                m_Way = new List<int>();
+                m_Queue = new Queue<int>();
+                m_Distance = new float[numberVertex];
+                for (int i = 0; i < m_Vertexes.Length; ++i)
+                {
+                    m_Vertexes[i] = new Vertex(i);
+                }
+            }
+
+            private void SetNonVisitedVertex()
+            {
+                for (int i = 0; i < m_Vertexes.Length; ++i)
+                {
+                    m_Vertexes[i].SetVisited(false);
+                }
+            }
+
+            public void AddEdge(int start, int end, float weight = 1)
+            {
+                m_Vertexes[start].AddEdge(end, weight);
+                if (m_Connected == EConnected.DOUBLY_CONNECTED)
+                {
+                    m_Vertexes[end].AddEdge(start, weight);
+                }
+            }
+
+            public void RemoveEdge(int start, int end)
+            {
+                m_Vertexes[start].RemoveEdge(end);
+                if (m_Connected == EConnected.DOUBLY_CONNECTED)
+                {
+                    m_Vertexes[end].RemoveEdge(start);
+                }
+            }
+
+            public Vertex GetVertex(int index)
+            {
+                return m_Vertexes[index];
+            }
+
+            public void Print()
+            {
+                for (int i = 0; i < m_Vertexes.Length; ++i)
+                {
+                    m_Vertexes[i].Print();
+                }
+            }
+
+            private bool CheckVertexVisited()
+            {
+                for (int i = 0; i < m_Vertexes.Length; ++i)
+                {
+                    if (!m_Vertexes[i].IsVisited())
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            private void SetVertexVisitedFalse()
+            {
+                for (int i = 0; i < m_Vertexes.Length; ++i)
+                {
+                    m_Vertexes[i].SetVisited(false);
+                }
+            }
+
+            private bool InternalDepthFirstSearch(int start, int end)
+            {
+                if (!m_Vertexes[start].IsVisited())
+                {
+                    m_Way.Add(start);
+                    m_Vertexes[start].SetVisited(true);
+                    if (start == end)
+                    {
+                        return true;
+                    }
+                    for (int i = 0; i < m_Vertexes[start].GetEdges().Count; ++i)
+                    {
+                        if (!m_Vertexes[m_Vertexes[start].GetEdges()[i].GetValue()].IsVisited())
+                        {
+                            if (InternalDepthFirstSearch(m_Vertexes[start].GetEdges()[i].GetValue(), end))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                    return false;
+                }
+                else
+                {
+                    m_Way.RemoveAt(m_Way.Count - 1);
+                    return false;
+                }
+            }
+
+            private List<int> GetWay()
+            {
+                return m_Way;
+            }
+
+            public List<int> DepthFirstSearch(int start, int end)
+            {
+                m_Way.Clear();
+                InternalDepthFirstSearch(start, end);
+                SetNonVisitedVertex();
+                var way = GetWay();
+                if (!way.Contains(end))
+                {
+                    way.Clear();
+                }
+                return way;
+            }
+
+            public List<int> BreadthFirstSearch(int start, int end)
+            {
+                m_Queue.Clear();
+                m_Queue.Enqueue(start);
+                m_Vertexes[start].SetVisited(true);
+                m_Vertexes[start].SetParent(-1);
+                while (m_Queue.Count != 0)
+                {
+                    var vertex = m_Queue.Peek();
+                    m_Queue.Dequeue();
+                    for (int i = 0; i < m_Vertexes[vertex].GetEdges().Count; ++i)
+                    {
+                        if (!m_Vertexes[m_Vertexes[vertex].GetEdges()[i].GetValue()].IsVisited())
+                        {
+                            m_Vertexes[m_Vertexes[vertex].GetEdges()[i].GetValue()].SetVisited(true);
+                            m_Queue.Enqueue(m_Vertexes[vertex].GetEdges()[i].GetValue());
+                            m_Vertexes[m_Vertexes[vertex].GetEdges()[i].GetValue()].SetParent(vertex);
+                        }
+                    }
+                }
+                SetNonVisitedVertex();
+                m_Way.Clear();
+                if (m_Vertexes[end].GetParent() == (-2))
+                {
+                    return m_Way;
+                }
+                int startParent = end;
+                while (!m_Vertexes[startParent].IsVisited())
+                {
+                    m_Way.Add(startParent);
+                    m_Vertexes[startParent].SetVisited(true);
+                    startParent = m_Vertexes[startParent].GetParent();
+                    if (startParent == (-1))
+                    {
+                        break;
+                    }
+                }
+                SetNonVisitedVertex();
+                m_Way.Reverse();
+                return m_Way;
+            }
+
+            public float[] Dijkstra(int start)
+            {
+                InitializeDistance();
+                m_Distance[start] = 0;
+                while (CheckVertexVisited())
+                {
+                    int currentVertex = GetMinDistanceIndex();
+                    for (int i = 0; i < m_Vertexes[currentVertex].GetEdges().Count; ++i)
+                    {
+                        var value = m_Vertexes[currentVertex].GetEdges()[i].GetWeight() + m_Distance[currentVertex];
+                        if (m_Distance[m_Vertexes[currentVertex].GetEdges()[i].GetValue()] > value)
+                        {
+                            m_Distance[m_Vertexes[currentVertex].GetEdges()[i].GetValue()] = value;
+                        }
+                    }
+                    m_Vertexes[currentVertex].SetVisited(true);
+                }
+                SetNonVisitedVertex();
+                return m_Distance;
+            }
+
+            public int GetMinDistanceIndex()
+            {
+                float minDistance = int.MaxValue;
+                int minIndex = 0;
+                for (int i = 0; i < m_Distance.Length; ++i)
+                {
+                    if ((m_Distance[i] < minDistance) && (!m_Vertexes[i].IsVisited()))
+                    {
+                        minDistance = m_Distance[i];
+                        minIndex = i;
+                    }
+                }
+                Console.WriteLine("minDistance: " + minDistance);
+                Console.WriteLine("minIndex: " + minIndex);
+                return minIndex;
+            }
+
+            public void InitializeDistance()
+            {
+                for (int i = 0; i < m_Distance.Length; ++i)
+                {
+                    m_Distance[i] = int.MaxValue;
+                }
+            }
+
+            public bool IsSetConnectedCount()
+            {
+                for (int i = 0; i < m_Vertexes.Length; ++i)
+                {
+                    if (m_Vertexes[i].GetConnectedCount() == 0)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+   
+            public int GetConnectedCount()
+            {
+                int connectedCount = 0;
+                while(IsSetConnectedCount())
+                {
+                    for (int i = 0; i < m_Vertexes.Length; ++i)
+                    {
+                        if (m_Vertexes[i].GetConnectedCount() == 0)
+                        {
+                            ++connectedCount;
+                            m_Vertexes[i].SetConnectedCount(connectedCount);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                        for (int j = 0; j < m_Vertexes.Length; ++j)
+                        {
+                            if (DepthFirstSearch(i, j).Count != 0)
+                            {
+                                m_Vertexes[j].SetConnectedCount(connectedCount);
+                            }
+                        }
+                    }
+                }
+                return connectedCount;
+            }
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructuresLibrary/AlgorithmsAndDataStructuresLibrary/LinearAlgebra/Vector.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsAndDataStructuresLibrary.LinearAlgebra
+{
+    namespace LinearAlgebra
+    {
+        public class Vector<T>
+        {
+            private List<T> m_values;
+
+            public Vector()
+            {
+                m_values = new List<T>();
+            }
+
+            public Vector(int size)
+            {
+                m_values = new List<T>();
+                for (int i = 0; i < size; ++i)
+                {
+                    m_values.Add(default(T));
+                }
+            }
+
+            public Vector(T[] values)
+            {
+                m_values = new List<T>();
+                for (int i = 0; i < values.Length; ++i)
+                {
+                    m_values.Add(values[i]);
+                }
+            }
+
+            public Vector(List<T> values)
+            {
+                m_values = new List<T>();
+                for (int i = 0; i < values.Count; ++i)
+                {
+                    m_values.Add(values[i]);
+                }
+            }
+
+            public void Print()
+            {
+                for (int i = 0; i < m_values.Count; ++i)
+                {
+                    Console.Write(m_values[i] + " ");
+                }
+            }
+
+            public int Count()
+            {
+                return m_values.Count;
+            }
+
+            public void SetValue(int index, T value)
+            {
+                m_values[index] = value;
+            }
+
+            public T GetValue(int index)
+            {
+                return m_values[index];
+            }
+
+            public double Length()
+            {
+                double result = 0;
+                for (int i = 0; i < m_values.Count; ++i)
+                {
+                    var vecValue = m_values[i] as dynamic;
+                    result += (vecValue * vecValue);
+                }
+                return Math.Sqrt(result);
+            }
+
+            public void AddValue(T value)
+            {
+                m_values.Add(value);
+            }
+
+            public T GetMaxValue()
+            {
+                T result = GetValue(0);
+                dynamic resultValue = result as dynamic;
+                for (int i = 1; i < Count(); ++i)
+                {
+                    dynamic value = GetValue(i) as dynamic;
+                    if (value > resultValue)
+                    {
+                        result = GetValue(i);
+                    }
+                }
+                return result;
+            }
+
+            public T GetMinValue()
+            {
+                T result = GetValue(0);
+                dynamic resultValue = result as dynamic;
+                for (int i = 1; i < Count(); ++i)
+                {
+                    dynamic value = GetValue(i) as dynamic;
+                    if (value < resultValue)
+                    {
+                        result = GetValue(i);
+                    }
+                }
+                return result;
+            }
+
+            public Vector<T> Reverse()
+            {
+                Vector<T> result = new Vector<T>();
+                for (int i = Count() - 1; i >= 0; --i)
+                {
+                    result.AddValue(m_values[i]);
+                }
+                return result;
+            }
+
+            public static Vector<T> operator +(Vector<T> vec1, Vector<T> vec2)
+            {
+                Vector<T> result = new Vector<T>();
+                for (int i = 0; i < vec1.Count(); ++i)
+                {
+                    var vec1Value = vec1.GetValue(i) as dynamic;
+                    var vec2Value = vec2.GetValue(i) as dynamic;
+                    result.AddValue(vec1Value + vec2Value);
+                }
+                return result;
+            }
+
+            public static Vector<T> operator -(Vector<T> vec1, Vector<T> vec2)
+            {
+                Vector<T> result = new Vector<T>();
+                for (int i = 0; i < vec1.Count(); ++i)
+                {
+                    var vec1Value = vec1.GetValue(i) as dynamic;
+                    var vec2Value = vec2.GetValue(i) as dynamic;
+                    result.AddValue(vec1Value - vec2Value);
+                }
+                return result;
+            }
+
+            public static Vector<T> operator *(Vector<T> vec, double value)
+            {
+                Vector<T> result = new Vector<T>();
+                for (int i = 0; i < vec.Count(); ++i)
+                {
+                    var vecValue = vec.GetValue(i) as dynamic;
+                    result.AddValue(vecValue * value);
+                }
+                return result;
+            }
+
+            public static Vector<T> operator /(Vector<T> vec, double value)
+            {
+                Vector<T> result = new Vector<T>();
+                for (int i = 0; i < vec.Count(); ++i)
+                {
+                    var vecValue = vec.GetValue(i) as dynamic;
+                    result.AddValue(vecValue / value);
+                }
+                return result;
+            }
+
+            public static double operator *(Vector<T> vec1, Vector<T> vec2)
+            {
+                double result = 0;
+                for (int i = 0; i < vec1.Count(); ++i)
+                {
+                    var vec1Value = vec1.GetValue(i) as dynamic;
+                    var vec2Value = vec2.GetValue(i) as dynamic;
+                    result += (vec1Value * vec2Value);
+                }
+                return result;
+            }
+
+            public static bool operator ==(Vector<T> vec1, Vector<T> vec2)
+            {
+                if (vec1.Count() == vec2.Count())
+                {
+                    for (int i = 0; i < vec1.Count(); ++i)
+                    {
+                        var vec1Value = vec1.GetValue(i) as dynamic;
+                        var vec2Value = vec2.GetValue(i) as dynamic;
+                        if (vec1Value != vec2Value)
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                return false;
+            }
+
+            public static bool operator !=(Vector<T> vec1, Vector<T> vec2)
+            {
+                if (vec1.Count() == vec2.Count())
+                {
+                    for (int i = 0; i < vec1.Count(); ++i)
+                    {
+                        var vec1Value = vec1.GetValue(i) as dynamic;
+                        var vec2Value = vec2.GetValue(i) as dynamic;
+                        if (vec1Value != vec2Value)
+                        {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+                return true;
+            }
+
+            public static Vector<T> operator !(Vector<T> vec)
+            {
+                Vector<T> result = new Vector<T>();
+                for (int i = 0; i < vec.Count(); ++i)
+                {
+                    var value = vec.GetValue(i) as dynamic;
+                    result.AddValue(-1 * value);
+                }
+                return result;
+            }
+
+            public static Vector<T> operator ++(Vector<T> vec)
+            {
+                Vector<T> result = new Vector<T>();
+                for (int i = 0; i < vec.Count(); ++i)
+                {
+                    var value = vec.GetValue(i) as dynamic;
+                    result.AddValue(++value);
+                }
+                return result;
+            }
+
+            public static Vector<T> operator --(Vector<T> vec)
+            {
+                Vector<T> result = new Vector<T>();
+                for (int i = 0; i < vec.Count(); ++i)
+                {
+                    var value = vec.GetValue(i) as dynamic;
+                    result.AddValue(--value);
+                }
+                return result;
+            }
+        }
+    }
+}
